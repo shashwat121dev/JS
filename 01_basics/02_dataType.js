9e7cf3784960448439667c4e9dadbd4ad86a3033
shashwat121dev <shashwat121.dev@gmail.com>
2026-07-02 08:39:05 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(2+3) We are using nodeJS not browser tabhi alert aata hai ise chalane pe
+
+console.log(2
+    +3); // Code readability honi chahiye 
+console.log("Shashwat");
+
+let name = "Shashwat"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// Numer -> 2 to power 53
+// bigint
+// string -> ""
+// boolean  -> True or False
+// null -> stand alone value
+// undefined -> value assign nahi hui hai abhi 
+// symbol -> Uniqueness ke liye use hota hai 
+
+// object 
+
+console.log(typeof null); // Null ek object type hai 
+console.log(typeof undefined); // Iska ek apna type hai Undefined 
+
+
+
+
+
+
