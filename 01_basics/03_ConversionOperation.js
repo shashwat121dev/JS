6156a05bdaeae3955330c116b78cbdd52461f4ee
shashwat121dev <shashwat121.dev@gmail.com>
2026-07-03 06:47:53 +0000

    Conversion Of DataType

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+let score = false
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> Nan
+// true -> 1
+// false -> 0
+
+let isLoggedIn = " " // True aa raha hai space dene pe aur na dene pe false aa jayega 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// "" -> false; "shashwat" -> true
+// " " -> true
+
+let a = 33; 
+
+let string = String(a)
+
+console.log(string);
+console.log(typeof string);
