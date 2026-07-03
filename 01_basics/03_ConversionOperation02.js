c962519b31ef6bc0190c513ddf51f8654af0d8a7
shashwat121dev <shashwat121.dev@gmail.com>
2026-07-03 08:27:07 +0000

    Conversion Operation 02

---
+++ 01_basics/03_ConversionOperation02.js
@@ -0,0 +1,24 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Shashwat"
+let str2 = " Sweetheart"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12 
+console.log("1" + 2 + 2); // 122 
+
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1 
+
+let GameCounter = 100; 
+GameCounter++; 
+
+console.log(GameCounter);
+
