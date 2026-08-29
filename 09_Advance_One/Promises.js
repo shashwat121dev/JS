5b5535a1ddee78bba8e673bd7ce75b66b8685a67
Shashwat Gupta <shashwat121.dev@gmail.com>
2026-08-29 13:53:31 +0530

    Fetch 02

--- 09_Advance_One/Promises.js
+++ 09_Advance_One/Promises.js
@@ -62,9 +62,52 @@ promiseFour
     return user.username
 })
 .then((username) => {
-    console.log(username);
+    // console.log(username); 
 })
 .catch(function(error){
     console.log(error);
+})// .finally(() => console.log("The Promise is either resolve or rejected"))
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+
+    let error = true;
+
+    if(!error){
+        resolve({username: "JavaScript", Password: "!@#$"})
+    }
+
+    else{
+        reject('Error: JS went wrong')
+    }
+    },2000)
 })
 
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+    console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+consumePromiseFive()
+
+
+async function getAllUsers(){
+    
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ",error);
+        
+    }
+    
+}
+
+getAllUsers()
